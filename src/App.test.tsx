695c6acd938f18389bc8ba734a40011d3195dd5d
Vinnie Rossi <VinnieRossi@users.noreply.github.com>
2022-05-07 09:42:42 -0400

    Remove unused css

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import { act, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
-import userEvent from '@testing-library/user-event';
-import axios from 'axios';
 
 jest.mock('axios');
 
